600a15ccd5144bad6c3bcd942fbaedd45d5f86db
Nam Nguyen <93068424+afacade@users.noreply.github.com>
2022-07-03 07:17:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import requests, json, random, datetime, asyncio, os, pygsheets
 
 #get environment var
 bot_token = os.getenv('BOT_TOKEN')
-ds_channel = int(os.getenv('DS_SERVER'))
-web_channel = int(os.getenv('WEB_SERVER'))
+ds_id = int(os.getenv('DS_SERVER'))
+web_id = int(os.getenv('WEB_SERVER'))
 
 
 #authorization
@@ -36,26 +36,47 @@ async def schedule_daily_message():
 	#get current time
 	now = datetime.datetime.now()
 	while True:
-    	#get latest message info 
-		row = df[(df['posted'] == '') & (df['discord_server'] == 'DS')].head(1)
-		row.time = pd.to_datetime(row.time)
-
+    		#get latest message info for DS
+		row_ds = df[(df['posted'] == '') & (df['discord_server'] == 'DS')].head(1)
+		row_ds.time = pd.to_datetime(row_ds.time)
+		
+		#get latest message info for WEB
+		row_web = df[(df['posted'] == '') & (df['discord_server'] == 'Web')].head(1)
+		row_web.time = pd.to_datetime(row_web.time)
+		
 		#get wait time
-		wait_time = abs((row.time-now).dt.total_seconds().values[0])
+		wait_time = abs((row_ds.time-now).dt.total_seconds().values[0])
 		print(f"Wait-time: {wait_time}")
 		await asyncio.sleep(wait_time)
-
-		#posting message
-		channel = bot.get_channel(ds_channel)
-		content = f"Hello Coderschool Learners, here is the challenge's link for today {row.url.values[0]}. Good luck!"
-		msg = await channel.send(content)
 		
-		#update spreadsheet before posting, E column for "posted"
-		cell = 'E' + str(row.id_challenge.values[0] + 1)
+		#--------------------------
+		
+		#posting message for DS
+		ds_channel = bot.get_channel(ds_id)
+		content = f"Hello Coderschool Learners, here is the challenge's link for today {row_ds.url.values[0]}. Good luck!"
+		msg = await channel.send(ds_channel)
+		
+		#update spreadsheet, E column for "posted"
+		cell = 'E' + str(row_ds.id_challenge.values[0] + 1)
 		sh.sheet1.update_value(cell, 'yes')
 		
 		#get message url and update spreadsheet H column for "message_url"
-		link_cell = 'F' + str(row.id_challenge.values[0] + 1)
+		link_cell = 'F' + str(row_ds.id_challenge.values[0] + 1)
+		sh.sheet1.update_value(link_cell, msg.jump_url)
+		
+		#--------------------------
+		
+		#posting message for WEB
+		web_channel = bot.get_channel(web_id)
+		content = f"test"
+		msg = await channel.send(content)
+		
+		#update spreadsheet, E column for "posted"
+		cell = 'E' + str(row_web.id_challenge.values[0] + 1)
+		sh.sheet1.update_value(cell, 'yes')
+		
+		#get message url and update spreadsheet H column for "message_url"
+		link_cell = 'F' + str(row_web.id_challenge.values[0] + 1)
 		sh.sheet1.update_value(link_cell, msg.jump_url)
 
 		
