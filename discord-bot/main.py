2e586e09f35d808c409821133ad492b2b2e8481e
Nam Nguyen <93068424+afacade@users.noreply.github.com>
2022-07-25 11:53:18 +0700

    Update main.py

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -59,7 +59,7 @@ async def schedule_daily_message():
 			#posting message for DS
 			ds_channel = bot.get_channel(ds_id)
 			embed = nextcord.Embed()
-			embed.description = f"Rules and info click here ->[here](https://discord.com/channels/957854915194126336/993340344198508615/993841605670871050). \n\n \
+			embed.description = f"RULES AND INFO CLICK [HERE](https://discord.com/channels/957854915194126336/993340344198508615/993841605670871050). \n\n \
 			Hello Coderschool Virgil DS Learners, as always the contest's link is {row_ds.url.values[0]} \n \
 			The challenge for today is ** {row_ds.challenge_used.values[0]} **. \n \
 			Difficulty ** {row_ds.difficulty.values[0]} ** \n \
@@ -81,7 +81,7 @@ async def schedule_daily_message():
 			#posting message for WEB
 			web_channel = bot.get_channel(web_id)
 			embed = nextcord.Embed()
-			embed.description = f"Rules and info click here ->[here](https://discord.com/channels/912307061310783538/993373092405583912/999348213486211162).\n\n\
+			embed.description = f"RULES AND INFO CLICK [HERE](https://discord.com/channels/912307061310783538/993373092405583912/999348213486211162).\n\n\
 			Hello Coderschool Virgil Web Learners, as always the contest's link is {row_web.url.values[0]} \n \
 			The challenge for today is ** {row_web.challenge_used.values[0]} **. \n \
 			Difficulty ** {row_web.difficulty.values[0]} ** \n \
